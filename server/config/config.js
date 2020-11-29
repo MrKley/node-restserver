740e6e0b37614ebe95de05a6c86f49ea5b7cb924
Kleyner <kvasconezc@unemi.edu.ec>
2020-11-28 23:22:53 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://mrkley:mamidorafenix1@cluster0.klvim.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
